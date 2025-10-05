dea8d5d80c10cfef7a9e4bfa6d75a80f6746fe70
leviethg <126061297+leviethg@users.noreply.github.com>
2025-10-05 18:07:48 +0700

    Add files via upload

---
+++ rag-chatbot/core/vector_store/vector_store.py
@@ -0,0 +1,20 @@
+from llama_index.core import VectorStoreIndex
+from dotenv import load_dotenv
+from ...setting import RAGSettings
+
+load_dotenv()
+
+
+class LocalVectorStore:
+    def __init__(
+        self,
+        host: str = "host.docker.internal",
+        setting: RAGSettings | None = None,
+    ) -> None:
+        self._setting = setting or RAGSettings()
+
+    def get_index(self, nodes):
+        if len(nodes) == 0:
+            return None
+        index = VectorStoreIndex(nodes=nodes)
+        return index
